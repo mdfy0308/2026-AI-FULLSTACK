3ea30ab066ee1189607c7732164987bc4fcc3e7d
sujeong <mdfy0308@gmail.com>
2026-09-22 18:16:44 +0900

    JS-Weather React Native project

---
+++ track012_ReactNative/JS-Weather/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>React Native를 배워보자~</Text>
+      <Text>오늘은 환경 세팅과 새로운 프로젝트를 생성하고 화면을 확인!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
